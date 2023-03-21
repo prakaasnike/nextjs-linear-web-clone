99c433222c2489036e93e65938bd376167a53aa7
Prakash Nayak <erprakashnayak@gmail.com>
2023-03-21 12:49:55 +0545

    basic config updated

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,11 +1,11 @@
 export default function Head() {
     return (
         <>
-            <title>Linear clone</title>
+            <title>Linear Website Clone</title>
             <meta content="width=device-width, initial-scale=1" name="viewport" />
             <meta
                 name="description"
-                content="Rebuilding Linear.app's website with Next.js, Tailwind and Framer Motion."
+                content="Rebuilding Linear website clone with Next.js, TailwindCss and Framer Motion."
             />
             <link rel="icon" href="/favicon.ico" />
         </>
