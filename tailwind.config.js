7ca33dd11d7f23fdec1c696b9b157f162b325a3d
Prakash Nayak <erprakashnayak@gmail.com>
2023-03-21 14:01:37 +0545

    created button with cva

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,38 +6,48 @@ module.exports = {
     './src/components/**/*.{tsx,jsx,js,ts}',
   ],
   theme: {
-    extend: {
-      colors: {
-        black: '#000212',
-        white: {
-          DEFAULT: '#fff',
-          100: 'rgba(255, 255, 255, 0.08)',
-        },
-      },
-      fontFamily: {
-        sans: '"SF Pro Display", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
-      },
-      fontSize: {
-        md: '1.6rem',
-        lg: ['2.2rem', '1.3'],
-        '5xl': ['8rem', '1'],
-      },
-      spacing: {
-        0: '0',
-        1: '0.4rem',
-        2: '0.8rem',
-        3: '1.2rem',
-        4: '1.6rem',
-        5: '2rem',
-        6: '2.4rem',
-        7: '2.8rem',
-        8: '3.2rem',
-        9: '3.6rem',
-        10: '4rem',
-        11: '4.4rem',
-        12: '4.8rem',
+    backgroundImage: {
+      'primary-gradient':
+        'linear-gradient(92.88deg, rgb(69, 94, 181) 9.16%, rgb(86, 67, 204) 43.89%, rgb(103, 63, 215) 64.72%)',
+    },
+    boxShadow: {
+      primary: 'rgb(80 63 205 / 50%) 0px 1px 40px',
+    },
+    colors: {
+      black: '#000212',
+      gray: '#858699',
+      white: {
+        DEFAULT: '#fff',
+        100: 'rgba(255, 255, 255, 0.08)',
       },
     },
+    fontFamily: {
+      sans: '"SF Pro Display", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
+    },
+    fontSize: {
+      xs: '1.3rem',
+      sm: '1.4rem',
+      md: '1.6rem',
+      lg: ['2.2rem', '1.3'],
+      '5xl': ['8rem', '1'],
+    },
+    spacing: {
+      0: '0',
+      1: '0.4rem',
+      2: '0.8rem',
+      3: '1.2rem',
+      4: '1.6rem',
+      5: '2rem',
+      6: '2.4rem',
+      7: '2.8rem',
+      8: '3.2rem',
+      9: '3.6rem',
+      10: '4rem',
+      11: '4.4rem',
+      12: '4.8rem',
+      'nav-height': 'var(--nav-height)',
+    },
+    extend: {},
   },
   plugins: [],
 };
