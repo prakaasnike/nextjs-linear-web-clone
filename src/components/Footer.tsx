822009dbe2a715ed1552d230c4a642a140dc2d5c
Prakash Nayak <erprakashnayak@gmail.com>
2023-03-22 12:19:49 +0545

    button header footer font size matched

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,14 +8,14 @@ import Link from 'next/link';
 
 export const Footer = () => {
     return (
-        <footer className="mt-12 border-t border-white-200 py-[5.6rem] text-sm">
+        <footer className="mt-12 border-t border-gray-100 py-[5.6rem] text-md">
             <Container className="flex flex-col justify-between lg:flex-row">
                 <div>
                     <div className="flex flex-row justify-between h-full lg:flex-col">
-                        <div className="flex items-center text-gray-100">
+                        <div className="flex items-center text-gray-300">
                             <Logo className="w-4 h-4 mr-4" /> Linear - Designed worldwide
                         </div>
-                        <div className="flex mt-auto space-x-4 text-gray-100">
+                        <div className="flex mt-auto space-x-4 text-gray-300">
                             <TwitterIcon />
                             <GithubIcon />
                             <SlackIcon />
@@ -29,7 +29,7 @@ export const Footer = () => {
                             <ul>
                                 {column.links.map((link) => (
                                     <li key={link.label} className="[&_a]:last:mb-0">
-                                        <Link href={link.href} className="block mb-3 text-gray-100 transition-colors hover:text-white-100">
+                                        <Link href={link.href} className="block mb-3 text-gray-300 transition-colors hover:text-white">
                                             {link.label}
                                         </Link>
                                     </li>
